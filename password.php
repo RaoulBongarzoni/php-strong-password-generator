787a1ea54cd43f0e5e22e6a0d987d1ed67ae2a71
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2023-12-25 21:05:41 +0100

    Standard Password Generator

--- password.php
+++ password.php
@@ -1,62 +1,74 @@
 <?php
 
-$lenght=$_GET["pswdLenght"] ?? false;
-$arrRepetitions=$_GET["pswdRepetitions"] ?? false;
-$options=[
-    "letters" =>$_GET["letters"] ?? true,
-    "numbers" =>$_GET["numbers"] ?? false,
-    "symbols" =>$_GET["symbols"] ?? false
+$pswdSourceString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,;.:-_?=/&%£!$";
+
+$lenght = $_GET["pswdLenght"] ?? false;
+$arrRepetitions = $_GET["pswdRepetitions"] ?? false;
+$options = [
+    "letters" => $_GET["letters"] ?? true,
+    "numbers" => $_GET["numbers"] ?? false,
+    "symbols" => $_GET["symbols"] ?? false
 ];
 
 
+function generateLenghtArray($len)
+{
+    $pswdSourceString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,;.:-_?=/&%£!$";
+    $pswdArraySource = str_split($pswdSourceString);
+    $pswdArray = [];
 
+    for ($i = 0; $i <= $len; ++$i) {
+        array_push($pswdArray, $pswdArraySource[rand(0, sizeof($pswdArraySource))]);
+    };
 
-function generateArray($options){
-    $sourceLetters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    $sourceNumbers="1234567890";
-    $sourceSymbols=",;.:-_?=/&%£!$";
-    $sourceArray=[];
-    if ($options[0]==true){
+    return implode($pswdArray);
+};
+
+/* function generateArray($options)
+{
+    $sourceLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $sourceNumbers = "1234567890";
+    $sourceSymbols = ",;.:-_?=/&%£!$";
+    $sourceArray = [];
+    if ($options[0] == true) {
         $sourceArray = array_merge($sourceArray, str_split($sourceLetters));
     }
-    if ($options[1]==true){
+    if ($options[1] == true) {
         $sourceArray = array_merge($sourceArray, str_split($sourceNumbers));
     }
-    if ($options[2]==true){
+    if ($options[2] == true) {
         $sourceArray = array_merge($sourceArray, str_split($sourceSymbols));
     }
 
     return $sourceArray;
-
 };
 
 function generatePassword($lenght, $repetitions, $source)
 {
-    
-
 };
-
+ */
 
 
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
 
-<h1> <?php 
-    $test=generateArray($options);
-    foreach ($element as $test){
-        echo $element;
-    };
+    <h1> <?php
 
-?> </h1>
-    
+            echo generateLenghtArray($lenght)
+
+            ?> </h1>
+
 </body>
+
 </html>
